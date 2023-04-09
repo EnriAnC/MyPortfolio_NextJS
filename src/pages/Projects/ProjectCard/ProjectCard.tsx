47e233dafeee1a055bdaadd8897ef26c82bfe1e3
EnriAnC <enriqueansa@gmail.com>
2023-04-09 17:54:18 -0400

    Working about, routes

---
+++ src/pages/Projects/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,9 @@
+const ProjectCard: React.FC = () => {
+    return (
+        <>
+            <h3>One project</h3>
+        </>
+    )
+}
+
+export default ProjectCard
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,7 +1,10 @@
+import ProjectCard from "./ProjectCard/ProjectCard"
+
 const Projects: React.FC = ()=>{
     return (
         <>
             <h2>from Projects</h2>
+            <ProjectCard />
         </>
     )
 }

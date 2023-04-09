0e3b42fb35cf4cf1243bac41a775c7b2f55979c1
EnriAnC <enriqueansa@gmail.com>
2023-04-09 17:48:37 -0400

    Working about, routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import Navbar from '@/components/common/Navbar/Navbar';
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {

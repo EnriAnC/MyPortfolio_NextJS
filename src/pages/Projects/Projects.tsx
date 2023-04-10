56081f6ef9f04425a9a3174ab31fa3a9e377235b
EnriAnC <enriqueansa@gmail.com>
2023-04-09 20:02:01 -0400

    Working about, routes

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,11 +1,12 @@
 import ProjectCard from "./ProjectCard/ProjectCard"
+import styles from './Projects.module.css'
 
 const Projects: React.FC = ()=>{
     return (
-        <>
+        <div className={ styles.projects }>
             <h2>from Projects</h2>
             <ProjectCard />
-        </>
+        </div>
     )
 }
 
